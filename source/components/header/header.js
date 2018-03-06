9207c3b3b0d6360bfb5a7daba406f4d9f84d09a8
Aleksey <foregestore@gmail.com>
2018-03-07 00:00:41 +0300

    50% mobile version

--- source/components/header/header.js
+++ source/components/header/header.js
@@ -1,2 +1,8 @@
 import './header.scss'
 import Typed from 'typed.js';
+
+$(document).ready(function () {
+    if(document.documentElement.clientWidth <= 900) {
+        $('.header-text > span').text('Создание современных и функциональных сайтов');
+    }
+});
